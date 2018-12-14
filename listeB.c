d117a5a8b80ec9456810b8820261a91332c9c132
Paul GEORGES <43345813+paulgeorges1998@users.noreply.github.com>
2018-12-15 00:51:48 +0100

    Update listeB.c

--- listeB.c
+++ listeB.c
@@ -78,7 +78,7 @@ ListeB tir_update(ListeB LB, ListeP LP, ListeR LR, ListeE *LE, int time, int v, 
       arme = tmp->data.arme;
       switch(arme){
       case 0:
-        if(time > tmp->data.tempsTirPrecedent + 250){
+        if(time > tmp->data.tempsTirPrecedent + 300){
           cons_listeB(&LB, nouvelle_balle(sprites[7], tmp->data.angle, 25, tmp->data.pos));
           Mix_PlayChannel(1, sons[0], 0);
           tmp->data.tempsTirPrecedent = time;
